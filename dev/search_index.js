fd19373ef2da6494efb22fe6bd2a473094a49b52
Documenter.jl <documenter@juliadocs.github.io>
2021-01-23 22:55:44 +0000

    build based on e10604e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BLASBenchmarksGPU","category":"page"},{"location":"#BLASBenchmarksGPU","page":"Home","title":"BLASBenchmarksGPU","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BLASBenchmarksGPU]","category":"page"}]
+}
