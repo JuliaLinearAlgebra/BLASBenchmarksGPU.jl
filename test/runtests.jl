719f2d5112b923be2e1c0a0456d2cd70f88e00d7
Dilum Aluthge <dilum@aluthge.com>
2021-01-23 19:10:12 -0500

    Remove unnecessary test deps (#6)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,8 @@ using BLASBenchmarksGPU
 using Test
 
 import InteractiveUtils
-import VectorizationBase
 
 include("test-suite-preamble.jl")
-
-@info("VectorizationBase.NUM_CORES is $(VectorizationBase.NUM_CORES)")
 
 @testset "BLASBenchmarksGPU.jl" begin
     @test BLASBenchmarksGPU.foo(1) == 2
