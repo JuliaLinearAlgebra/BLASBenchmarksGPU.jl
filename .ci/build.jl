073e630c044a0b1fd9fafea94cf4e7ce079385d8
Dilum Aluthge <dilum@aluthge.com>
2021-01-31 22:50:24 -0500

    A few style tweaks, and use the registered version of GemmKernels.jl

--- .ci/build.jl
+++
@@ -1,8 +0,0 @@
-using Pkg
-
-deps = [
-    Pkg.PackageSpec(url = "https://github.com/JuliaGPU/GemmKernels.jl", rev = "master"),
-]
-Pkg.add(deps)
-Pkg.build()
-Pkg.precompile()
--- src/benchconfig.jl
+++ src/benchconfig.jl
@@ -21,11 +21,22 @@ function getbenchfunc(lib::Symbol)
     throw(ArgumentError("Unknown library \"$(lib)\""))
 end
 
+function gemm_cublas!(C, A, B)
+    CUDA.CUBLAS.cublasSetMathMode(CUDA.CUBLAS.handle(), CUDA.CUBLAS.CUBLAS_TENSOR_OP_MATH)
+    CUDA.CUBLAS.gemmEx!('N', 'N', 1, A, B, 0, C)
+    return C
+end
+
 function benchmark_gemm_cublas!(C, A, B)
     CUDA.CUBLAS.cublasSetMathMode(CUDA.CUBLAS.handle(), CUDA.CUBLAS.CUBLAS_TENSOR_OP_MATH)
     return BenchmarkTools.@benchmark CUDA.@sync begin
         CUDA.CUBLAS.gemmEx!('N', 'N', 1, $A, $B, 0, $C)
     end
+end
+
+function gemm_gemmkernels!(C, A, B)
+    GemmKernels.BLAS.gemmEx!('N', 'N', 1, A, B, 0, C)
+    return C
 end
 
 function benchmark_gemm_gemmkernels!(C, A, B)
@@ -34,7 +45,10 @@ function benchmark_gemm_gemmkernels!(C, A, B)
     end
 end
 
-gemm_tullio!(C, A, B) = @tullio C[i, j] = A[i, k] * B[k, j]
+function gemm_tullio!(C, A, B)
+    Tullio.@tullio C[i, j] = A[i, k] * B[k, j] cuda=256 grad=false
+    return C
+end
 
 function benchmark_gemm_tullio!(C, A, B)
     return BenchmarkTools.@benchmark CUDA.@sync begin
